9c3e105aea0aee4981a08dfe9a4f8a14a181d945
sungyeh <kido6115@hotmal.com>
2019-03-05 09:17:16 +0800

    新增freemarker範例

---
+++ src/main/java/com/tist/controller/CityController.java
@@ -0,0 +1,30 @@
+package com.tist.controller;
+
+import com.tist.domain.City;
+import com.tist.service.CityService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+
+@Controller
+public class CityController {
+    @Resource
+    private CityService cityService;
+
+    @RequestMapping("/hello")
+    public String hello(Model model) {
+        City city = new City();
+        model.addAttribute("msg", "Hello Tiffany , this is freemarker");
+        model.addAttribute("city", city);
+        return "hello";
+    }
+
+    @RequestMapping("/form-test")
+    public String formInput(City city, Model model) {
+        System.out.println(city);
+        model.addAttribute("list", cityService.findAll());
+        return "hello";
+    }
+}
